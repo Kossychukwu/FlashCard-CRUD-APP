c3e06b990194562efe5a76a6101c89ff1682c18b
Kossychukwu <kossydike@gmail.com>
2023-02-02 15:45:08 +0100

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,27 @@
+import express, {Request, Response} from 'express';
+import mongoose from 'mongoose';
+import Deck from "./models/Deck"
+
+const PORT  = 5000
+const app = express();
+app.use(express.json())
+
+
+app.post('/decks', async(req: Request, res: Response)=>{
+    // res.send('hello user');
+    console.log(req.body)
+    const newDeck = new Deck({
+        title: req.body.title,
+    })
+    const CreatedDeck = await newDeck.save();
+    res.json(CreatedDeck)
+})
+
+
+mongoose.set('strictQuery', true)
+mongoose.connect("mongodb+srv://Flashcard:Wku9JgWJ70RBoXg8@cluster1.llz1ikd.mongodb.net/?retryWrites=true&w=majority").then(() => {
+console.log(`listening on port ${PORT}`)
+ app.listen(PORT)
+})
+
+// password Wku9JgWJ70RBoXg8
